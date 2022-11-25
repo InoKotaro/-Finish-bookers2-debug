9ad18c3c7adb3954e28d5f6024b6d34ab266031c
InoKotaro <114914604+InoKotaro@users.noreply.github.com>
2022-11-25 06:20:17 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @book = Book.new
